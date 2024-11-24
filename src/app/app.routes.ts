910eee505afea1af145bf749949a0c3e0d6d08c4
Mitchell <mitchellwaynemorris@gmail.com>
2024-11-24 11:54:41 -0800

    deleted current-list as it's not needed for now

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,11 @@
 import { Routes } from '@angular/router';
 import { AppLayoutComponent } from './layout/app.layout.component';
 import { LandingComponent } from './landing/landing.component';
-import { CurrentListComponent } from './current-list/current-list.component';
 
 export const routes: Routes = [
 	{
 		path: '', component: AppLayoutComponent, children: [
 			{ path: '', component: LandingComponent },
-			// { path: 'current-list', component: CurrentListComponent },
 			{ path: 'tasklist', data: { breadcrumb: 'Task List' }, loadChildren: () => import('./tasklist/tasklist.app.module').then(m => m.TaskListAppModule) },
 		]
 	}
